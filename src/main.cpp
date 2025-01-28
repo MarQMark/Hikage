3742603b93d750f6889457a70a61aaf0cae486e1
mark <kugelflash@gmail.com>
2025-01-28 17:57:03 +0100

    Add dearImGui Add glew Add glm Add json lib Add basic Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui_impl_glfw.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    if (!glfwInit())
+        std::cout << "ERROR: Could not initialize GLFW" << std::endl;
+
+    auto* window = glfwCreateWindow(1280, 720, "Hikage", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        std::cout << "ERROR: Could create Window" << std::endl;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    //glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+
+    glfwSwapInterval(0);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "ERROR: Could not initialize GLEW" << std::endl;
+
+    // Setup ImGUI
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 430");
+
+    while(!glfwWindowShouldClose(window)){
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        bool show_demo_window = true;
+        ImGui::ShowDemoWindow(&show_demo_window);
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     return 0;
 }
